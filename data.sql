a7b706bc222372f85aed4b07fa11abb3ed144277
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-26 13:45:13 -0700

    feat: Create a table named animal with different columns

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (1, 'Agumon', '02/3/2020', 0, true, 10.23);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (2, 'Gabumon', '11/15/2018', 2, true, 8);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (3, 'Pikachu', '01/7/2012', 1, false, 15.04);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (4,'Devimon', '05/12/2017', 5, false, 11);
