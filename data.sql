2f9a38bf8abfe591094e8814cf2fd7e77a4b7414
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-27 06:11:31 -0700

    feat: Insert new data

--- data.sql
+++ data.sql
@@ -8,3 +8,15 @@ INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
 VALUES (3, 'Pikachu', '01/7/2012', 1, false, 15.04);
 INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
 VALUES (4,'Devimon', '05/12/2017', 5, false, 11);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (5,'Charmander', '02/8/2020', 0, false, -11);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (6,'Plantmon', '11/15/2022', 2, true, -5.7);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (7,'Squirtle', '04/2/2020', 3, false, -12.13);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (8,'Angemon', '06/12/2015', 1, true, -45);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (9,'Boarmon', '06/7/2005', 7, true, 20.4);
+INSERT INTO animals (id, name, dateOfBirth, escapeAttempts, neutered, weightKg)
+VALUES (10,'Blossom', '10/13/1998', 3, true, 17);
