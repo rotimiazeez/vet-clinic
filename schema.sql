92ce349c29de4a9770e1f0c49d1a91d03cae4b0b
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-27 06:13:11 -0700

    feat: Modify schema.sql

--- schema.sql
+++ schema.sql
@@ -2,9 +2,10 @@
 
 CREATE TABLE animals (
   id INT NOT NULL,
-	name VARCHAR(25),
-	dateOfBirth date,
-	escapeAttempts INT,
-	neutered BOOLEAN,
-	weightKg DECIMAL
+  name VARCHAR(25),
+  dateOfBirth date,
+  escapeAttempts INT,
+  neutered BOOLEAN,
+  weightKg DECIMAL,
+  species VARCHAR(40);
 );
