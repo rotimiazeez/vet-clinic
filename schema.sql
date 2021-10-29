807e7fb20af5ed1b9d7d6dc68c8b927f47343085
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-28 17:21:19 -0700

    feat: Add new table to database

--- schema.sql
+++ schema.sql
@@ -29,4 +29,21 @@ DROP COLUMN species;
 ALTER TABLE animals ADD species_id int REFERENCES species(id);
 ALTER TABLE animals ADD owner_id int REFERENCES owners(id);
 
-  
+   CREATE TABLE vets (
+   id SERIAL,
+   name varchar(30),
+   age int,
+   date_of_graduation date,
+   PRIMARY KEY (id)
+   );
+
+    CREATE TABLE specializations (
+   species_id int,
+   vet_id int
+   );
+
+ CREATE TABLE visits (
+   animal_id int,
+   vet_id int,
+   visitDate date
+   );
